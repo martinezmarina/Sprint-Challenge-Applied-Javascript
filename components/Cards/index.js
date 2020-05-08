5629a69760cef0b4f85b0526595dbab7d5ae7b0e
martinezmarina <martinezmarina980@gmail.com>
2020-05-08 13:16:45 -0400

    all cards created

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,66 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+const cardsEntry = document.querySelector('.cards-container')
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then(response => {
+    const bootstrapArray = response.data.articles.bootstrap
+    const javascriptArray = response.data.articles.javascript
+    const jqueryArray = response.data.articles.jquery
+    const nodeArray = response.data.articles.node 
+    const technologyArray = response.data.articles.technology 
+
+    bootstrapArray.forEach(item => {
+        const article = cardCreator(item)
+        cardsEntry.appendChild(article)
+    });
+    javascriptArray.forEach(item => {
+        const article = cardCreator(item)
+        cardsEntry.appendChild(article)
+    });
+    jqueryArray.forEach(item => {
+        const article = cardCreator(item)
+        cardsEntry.appendChild(article)
+    });
+    nodeArray.forEach(item => {
+        const article = cardCreator(item)
+        cardsEntry.appendChild(article)
+    });
+    technologyArray.forEach(item => {
+        const article = cardCreator(item)
+        cardsEntry.appendChild(article)
+    });
+
+})
+.catch(error => {
+    console.log("failed")
+})
+
+function cardCreator (attrs){
+    const {authorName, authorPhoto, headline} = attrs
+
+    const cardContainer = document.createElement('div')
+    const headlineTitle = document.createElement('div')
+    const authorContainer = document.createElement('div')
+    const imageContainer = document.createElement('div')
+    const authorImage = document.createElement('img')
+    const author = document.createElement('span')
+
+    cardContainer.classList.add('card')
+    headlineTitle.classList.add('headline')
+    headlineTitle.textContent = headline
+    authorContainer.classList.add('author')
+    imageContainer.classList.add('img-container')
+    authorImage.src = authorPhoto
+    author.textContent = `By ${authorName}`
+
+    cardContainer.appendChild(headlineTitle)
+    cardContainer.appendChild(authorContainer)
+    authorContainer.appendChild(imageContainer)
+    authorContainer.appendChild(author)
+    imageContainer.appendChild(authorImage)
+
+    return cardContainer
+}
