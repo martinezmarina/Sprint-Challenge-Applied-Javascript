fb0e3112db9caa5abe5bd798b1cc3ae00a7fdc37
martinezmarina <martinezmarina980@gmail.com>
2020-05-08 11:58:11 -0400

    header completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,26 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function header() {
+    const headerContainer = document.createElement('div')
+    const date = document.createElement('span')
+    const header = document.createElement('h1')
+    const temperature = document.createElement('span')
+
+    headerContainer.classList.add("header")
+    date.classList.add("date")
+    temperature.classList.add("temp")
+    date.textContent = "MARCH 28, 2019"
+    header.textContent = "Lambda Times"
+    temperature.textContent = "98°"
+
+    headerContainer.appendChild(date)
+    headerContainer.appendChild(header)
+    headerContainer.appendChild(temperature)
+
+    const headerEntry = document.querySelector('.header-container')
+    headerEntry.appendChild(headerContainer)
+   
+}
+header()
+
